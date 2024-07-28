d698c85ab09b1747e105e757829a900a0293ac90
Rudra Prasad <rudraofficial202@gmail.com>
2024-07-28 15:05:21 +0530

    new route data

--- app/colleges/page.tsx
+++ app/colleges/page.tsx
@@ -1,23 +1,6 @@
 "use client";
 import ShowCard from "@/components/showCard";
-
-const colleges = [
-  {
-    name: "ROLAND INSTITUTE OF TECHNOLOGY",
-    location: "BERHAMPUR",
-    id: "ACEANG",
-  },
-  {
-    name: "VIGNAN INSTITUTE OF TECHNOLOGY",
-    location: "BERHAMPUR",
-    id: "ABITCTC",
-  },
-  {
-    name: "KALAM INSTITUTE OF TECHNOLOGY",
-    location: "BERHAMPUR",
-    id: "AIETBBSR",
-  },
-];
+import { colleges } from "@/data/list";
 
 const CollegesPage = () => {
   return (
---
+++ data/list.tsx
@@ -0,0 +1,17 @@
+export const colleges = [
+    {
+      name: "ROLAND INSTITUTE OF TECHNOLOGY",
+      location: "BERHAMPUR",
+      id: "ACEANG",
+    },
+    {
+      name: "VIGNAN INSTITUTE OF TECHNOLOGY",
+      location: "BERHAMPUR",
+      id: "ABITCTC",
+    },
+    {
+      name: "KALAM INSTITUTE OF TECHNOLOGY",
+      location: "BERHAMPUR",
+      id: "AIETBBSR",
+    },
+  ];
