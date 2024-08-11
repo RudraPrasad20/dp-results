e10cdba0d0c43925edd9c4d9282feb1a28afbe50
Rudra Prasad <rudraofficial202@gmail.com>
2024-08-11 12:57:37 +0530

    more data

--- data/list.tsx
+++ data/list.tsx
@@ -1,17 +1,132 @@
 export const colleges = [
-    {
-      name: "ROLAND INSTITUTE OF TECHNOLOGY",
-      location: "BERHAMPUR",
-      id: "ACEANG",
-    },
-    {
-      name: "VIGNAN INSTITUTE OF TECHNOLOGY",
-      location: "BERHAMPUR",
-      id: "ABITCTC",
-    },
-    {
-      name: "KALAM INSTITUTE OF TECHNOLOGY",
-      location: "BERHAMPUR",
-      id: "AIETBBSR",
-    },
-  ];
+  {
+    name: "ADARSHA COLLEGE OF ENGINEERING",
+    location: "ANUGUL",
+    id: "ACEANG",
+  },
+  {
+    name: "AJAY BINAY INSTITUTE OF TECHNOLOGY",
+    location: "CUTTACK",
+    id: "ABITCTC",
+  },
+  {
+    name: "ARYAN INSTITUTE OF ENGINEERING AND TECHNOLOGY",
+    location: "BHUBANESWAR",
+    id: "AIETBBSR",
+  },
+  {
+    name: "BALASORE INSTITUTE OF ENGINEERING AND TECHNOLOGY",
+    location: "BALASORE",
+    id: "BIETBLS",
+  },
+  {
+    name: "BHADRAK INSTITUTE OF OF ENGINEERING AND TECHNOLOGY",
+    location: "BHADRAK",
+    id: "BIETBDRK",
+  },
+  {
+    name: "BHUBANESWAR COLLEGE OF ENGINEERING",
+    location: "BHUBANESWAR",
+    id: "BCEBBSR",
+  },
+  {
+    name: "BHUBANESWAR ENGINEERING COLLEGE",
+    location: "BHUBANESWAR",
+    id: "BECBBSR",
+  },
+  {
+    name: "BHUBANESWAR INSTITUTE OF INDUSTRIAL TECHNOLOGY",
+    location: "KHURDA",
+    id: "BIITKHR",
+  },
+  {
+    name: "BHUBANESWAR INSTITUTE OF TECHNOLOGY",
+    location: "BHUBANESWAR",
+    id: "BITBBSR",
+  },
+  {
+    name: "BLACK DIMOND INSTITUTE OF ENGINEERING & TECHNOLOGY",
+    location: "JHARSUGUDA",
+    id: "BDIETJSGD",
+  },
+  {
+    name: "CAPITAL ENGINNERING COLLEGE",
+    location: "BHUBANESWAR",
+    id: "CECBBSR",
+  },
+  {
+    name: "CENTRAL INSTITUTE OF PETROCHEMICALS ENGINEERING & TECHNOLOGY",
+    location: "BHUBANESWAR",
+    id: "CIPETBBSR",
+  },
+  {
+    name: "CENTRE FOR UG & PG STUDIES BPUT",
+    location: "ROURKELA",
+    id: "CUPSBPUTRKLA",
+  },
+  {
+    name: "COLLEGE OF ENGINEERING",
+    location: "BHUBANESWAR",
+    id: "CEBBSR",
+  },
+  // {
+  //   name: "DRIEMS",
+  //   location: "CUTTACK",
+  //   id: "DCTC",
+  // },
+  {
+    name: "EINSTEIN ACADEMY OF TECHNOLOGY & MANAGEMENT",
+    location: "BHUBANESWAR",
+    id: "EATMBBSR",
+  },
+  // {
+  //   name: "GANDHI ENGINEERING COLLEGE",
+  //   location: "BHUBANESWAR",
+  //   id: "GECBBSR",
+  // },
+  {
+    name: "GANDHI INSTITUTE FOR EDUCATION & TECHNOLOGY",
+    location: "KHURDA",
+    id: "GIETKHR",
+  },
+  // {
+  //   name: "GANDHI INSTITUTE FOR TECHNOLOGICAL ADVANCEMENT",
+  //   location: "BHUBANESWAR",
+  //   id: "GITABBSR",
+  // },
+  // {
+  //   name: "GANDHI INSTITUTE FOR TECHNOLOGY",
+  //   location: "BHUBANESWAR",
+  //   id: "GITBBSR",
+  // },
+  {
+    name: "GANDHI INSTITUTE OF ADVANCED COMPUTER & RESEARCH",
+    location: "RAYAGADA",
+    id: "GITACRRGDA",
+  },
+  {
+    name: "GANDHI INSTITUTE OF EXCELLENT TECHNOCRATS",
+    location: "BHUBANESWAR",
+    id: "GIETBBSR",
+  },
+  // {
+  //   name: "GANDHI INSTITUTE OF SCIENCE & TECHNOLOGY",
+  //   location: "RAYAGADA",
+  //   id: "GISATRGDA",
+  // },
+  // {
+  //   name: "GANDHI INSTITUTE OF TECHNOLOGY AND MANAGEMENT",
+  //   location: "BHUBANESWAR",
+  //   id: "GITMBBSR",
+  // },
+  // {
+  //   name: "GANDHI ACADEMY OF TECHNOLOGY & MANAGEMENT",
+  //   location: "BERHAMPUR",
+  //   id: "GATEBER",
+  // },
+  // {
+  //   name: "GAYATRI COLLEGE OF ENGINEERING",
+  //   location: "BERHAMPUR",
+  //   id: "GCEBER",
+  // },
+];
