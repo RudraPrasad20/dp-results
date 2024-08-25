5def185cbe565b03128530b21228d06193f10b8b
Rudra Prasad <rudraofficial202@gmail.com>
2024-08-25 22:59:24 +0530

    added all data

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import { ModeToggle } from "@/components/modeToggle";
+import CommandSearch from "@/components/searchbar";
+import { colleges } from "@/data/list";
 import React from "react";
 
+
 const page = () => {
+  const commands = colleges.map(item => ({
+    value: item.id,
+    label: item.name,
+  }));
+
+  
   return (
     <div>
       Results{" "}
+      
       <div>
         <ModeToggle />
-      </div>
+      </div> <CommandSearch commands={commands} />;
     </div>
   );
 };
--- data/list.tsx
+++ data/list.tsx
@@ -190,4 +190,209 @@ export const colleges = [
     location: "BHUBANESWAR",
     id: "MPBBSR",
   },
+  {
+    name: "MAHAVIR INSTITUTE OF ENGINEERING TECHNOLOGY",
+    location: "BHUBANESWAR",
+    id: "MIETBBSR",
+  },
+  {
+    name: "MAJHIGHARIANI INSTITUTE OF TECHNOLOGY & SCIENCE",
+    location: "RAYAGADA",
+    id: "MITSRRGDA",
+  },
+  {
+    name: "MODERN ENGINEERING & MANAGEMENT STUDIES",
+    location: "BALASORE",
+    id: "MEMSBLS",
+  },
+  {
+    name: "MODERN INSTITUTE OF TECHNOLOGY & MANAGEMENT",
+    location: "BHUBANESWAR",
+    id: "MITMBBSR",
+  },
+  {
+    name: "NALANDA INSTITUTE OF TECHNOLOGY",
+    location: "BHUBANESWAR",
+    id: "NITBBSR",
+  },
+  {
+    name: "NIGAM INSTITUTE OF ENGINEERING & TECHNOLOGY",
+    location: "BARANG",
+    id: "NIETBRN",
+  },
+  {
+    name: "NIIS INSTITUTE OF ENGINEERING & TECHNOLOGY",
+    location: "BHUBANESWAR",
+    id: "NIETBBSR",
+  },
+  {
+    name: "NATIONAL INSTITUTE OF SCIENCE & TECHNOLOGY",
+    location: "BERHAMPUR",
+    id: "NISTBER",
+  },
+  {
+    name: "NM INSTITUTE OF ENGINEERING & TECHNOLOGY",
+    location: "BHUBANESWAR",
+    id: "NMIETBBSR",
+  },
+  {
+    name: "OXFORD COLLEGE OF ENGINEERING & MANAGEMENT",
+    location: "BHUBANESWAR",
+    id: "OCEMBBSR",
+  },
+  {
+    name: "PADMASHREE KRUTARTHA ACHARYA COLLEGE OF ENGINEERING",
+    location: "BARGARH",
+    id: "PKACEBGR",
+  },
+  {
+    name: "PARALA MAHARAJA ENGINEERING COLLEGE",
+    location: "BERHAMPUR",
+    id: "PMECBER",
+  },
+  {
+    name: "PILOO MODY COLLEGE OF ARCHITECTURE",
+    location: "CUTTACK",
+    id: "PMCAACTC",
+  },
+  {
+    name: "RADHA KRISHNA INSTITUTE OF TECHNOLOGY & ENGINEERING",
+    location: "BHUBANESWAR",
+    id: "RITEBBSR",
+  },
+  {
+    name: "RAJDHANI ENGINEERING COLLEGE",
+    location: "BHUBANESWAR",
+    id: "RECBBSR",
+  },
+  {
+    name: "RAYAGADA INSTITUTE OF TECHNOLOGY & MANAGEMENT",
+    location: "RAYAGADA",
+    id: "RITMRGDA",
+  },
+  {
+    name: "ROLAND INSTITUTE OF TECHNOLOGY",
+    location: "BERHAMPUR",
+    id: "RITBER",
+  },
+  {
+    name: "SAMANTA CHANDRA SEKHAR INSTITUTE OF TECHNOLOGY & MANAGEMENT",
+    location: "SIMILIGUDA",
+    id: "SCSITMSLG",
+  },
+  {
+    name: "SANJAY MEMORIAL INSTITUTE OF TECHNOLOGY",
+    location: "BERHAMPUR",
+    id: "SMITBER",
+  },
+  {
+    name: "SATYASAI ENGINEERING COLLEGE",
+    location: "BALASORE",
+    id: "SECBLS",
+  },
+  {
+    name: "SEEMANTA ENGINEERING COLLEGE",
+    location: "JHARAPOKHARIA",
+    id: "SECJPK",
+  },
+  {
+    name: "SGI SCHOOL OF ARCHITECH",
+    location: "CUTTACK",
+    id: "SSACTC",
+  },
+  {
+    name: "SILICON INSTITUTE OF TECHNOLOGY",
+    location: "BHUBANESWAR",
+    id: "SITBBSR",
+  },
+  {
+    name: "SILICON INSTITUTE OF TECHNOLOGY",
+    location: "SAMBALPUR",
+    id: "SITSBL",
+  },
+  {
+    name: "SEEMANTA ENGINEERING COLLEGE",
+    location: "JHARAPOKHARIA",
+    id: "SECKHR",
+  },
+  {
+    name: "SOPHITORIUM ENGINEERING COLLEGE",
+    location: "KHURDA",
+    id: "SECJPK",
+  },
+  {
+    name: "SPINTRONIC TECHNOLOGY & ADVANCE RESEARCH",
+    location: "KHURDA",
+    id: "STARKHR",
+  },
+  {
+    name: "SRINIX COLLEGE OF ENGINEERING",
+    location: "BALASORE",
+    id: "SCEBLS",
+  },
+  {
+    name: "SUBAS INSTITUTE OF TECHNOLOGY",
+    location: "BARANG",
+    id: "SITBRN",
+  },
+  {
+    name: "SUDDHANANDA ENGINEERING & RESEACH CENTRE",
+    location: "CUTTACK",
+    id: "SERCCTC",
+  },
+  {
+    name: "SUNDARGARH ENGINEERING COLLEGE",
+    location: "SUNDARGARH",
+    id: "SECSRG",
+  },
+  {
+    name: "SYNERGY INSTITUTE OF ENGINEERING & TECHNOLOGY",
+    location: "DHENKANAL",
+    id: "SIETDKL",
+  },
+  {
+    name: "SYNERGY INSTITUTE OF TECHNOLOGY",
+    location: "PAHALA",
+    id: "SITPHL",
+  },
+  {
+    name: "TEMPLECITY INSTITUTE OF TECHNOLOGY & ENGINEERING",
+    location: "BHUBANESWAR",
+    id: "TITEBBSR",
+  },
+  {
+    name: "THE TECHNO SCHOOL",
+    location: "BHUBANESWAR",
+    id: "TTSBBSR",
+  },
+  {
+    name: "TRIDENT ACADEMY OF TECHNOLOGY",
+    location: "BHUBANESWAR",
+    id: "TATBBSR",
+  },
+  {
+    name: "VEDANG INSTITUTE OF TECHNOLOGY KHURDA",
+    location: "KHURDA",
+    id: "VITKHR",
+  },
+  {
+    name: "VIGNAN INSTITUTE OF TECHNOLGY & MANAGEMENT",
+    location: "BERHAMPUR",
+    id: "VITMBER",
+  },
+  {
+    name: "VIJAYANJALI INSTITUTE OF TECHNOLOGY",
+    location: "BALASORE",
+    id: "VITBLS",
+  },
+  {
+    name: "VIKASH INSTITUTE OF TECHNOLOGY",
+    location: "BARGARH",
+    id: "VITBRG",
+  },
+  {
+    name: "VITS ENGINEERING COLLEGE",
+    location: "KHURDA",
+    id: "VECKHR",
+  },
 ];
